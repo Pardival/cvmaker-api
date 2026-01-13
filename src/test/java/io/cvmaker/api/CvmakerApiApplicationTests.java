dc21a4916309cfd535704b6b6e55bf6922fc244f
Kevin Dufour <kevin.dufourpro@gmail.com>
2026-01-13 22:01:27 +0100

    inital commit

---
+++ src/test/java/io/cvmaker/api/CvmakerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.cvmaker.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvmakerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
