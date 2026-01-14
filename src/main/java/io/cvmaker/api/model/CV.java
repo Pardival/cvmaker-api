92af7bc728f233159d7e9b4deb09055230f20dfe
Kevin Dufour <kevin.dufourpro@gmail.com>
2026-01-14 22:37:41 +0100

    Fill CV

--- src/main/java/io/cvmaker/api/model/CV.java
+++ src/main/java/io/cvmaker/api/model/CV.java
@@ -1,4 +1,29 @@
 package io.cvmaker.api.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Document(collection = "cvs")
 public class CV {
+    @Id
+    private String id;
+    private String userId;
+    private String cvName;
+    private String summary;
+
+    private PersonalDetails personalDetails;
+    private List<Education> education;
+    private List<Experience> experience;
+    private List<Skill> skills;
+    private List<Language> languages;
 }
