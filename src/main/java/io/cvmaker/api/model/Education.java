922ab1da99fe0cf08ba97a799c03609ed07e1385
Kevin Dufour <kevin.dufourpro@gmail.com>
2026-01-14 21:56:43 +0100

    Fill Education entity

--- src/main/java/io/cvmaker/api/model/Education.java
+++ src/main/java/io/cvmaker/api/model/Education.java
@@ -1,4 +1,22 @@
 package io.cvmaker.api.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Education {
+    String institution;
+    String degree;
+    List<String> achievements;
+    String Location;
+    LocalDate startYear;
+    LocalDate endYear;
 }
