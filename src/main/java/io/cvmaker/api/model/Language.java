4101e9befea32899f270ff0ee9f9c0999bc7d6c1
Kevin Dufour <kevin.dufourpro@gmail.com>
2026-01-14 22:15:19 +0100

    Fill Language

---
+++ src/main/java/io/cvmaker/api/model/Language.java
@@ -0,0 +1,15 @@
+package io.cvmaker.api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Language {
+    private String language;
+    private String level;
+}
