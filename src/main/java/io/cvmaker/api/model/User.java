fe59c0d42185cdeace31374b07fbe09f6e950681
Kevin Dufour <kevin.dufourpro@gmail.com>
2026-01-15 19:59:22 +0100

    Fill User entity

--- src/main/java/io/cvmaker/api/model/User.java
+++ src/main/java/io/cvmaker/api/model/User.java
@@ -1,4 +1,32 @@
 package io.cvmaker.api.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Document(collection = "users")
 public class User {
+    @Id
+    private String id;
+
+    @Indexed(unique = true)
+    private String email;
+
+    private String username;
+    private String googleId;
+    private String phoneNumber;
+    private String profilePictureUrl;
+    private LocalDateTime createdAt;
+    private LocalDateTime lastLogin;
+
 }
